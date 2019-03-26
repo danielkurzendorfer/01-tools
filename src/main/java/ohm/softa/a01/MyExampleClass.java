08bd69200e6601512cdbe9edc98ebe8085c6a58e
Daniel Kurzendorfer <daniel.kurzendorfer@gmail.com>
2019-03-26 09:16:31 +0100

    Musterloesung test

--- src/main/java/ohm/softa/a01/MyExampleClass.java
+++ src/main/java/ohm/softa/a01/MyExampleClass.java
@@ -5,7 +5,5 @@ class MyExampleClass {
 	 * Basic integer multiplication
 	 * @return product of a and b
 	 */
-    int multiply(int a, int b) {
-		return a * b;
-	}
+    int multiply(int a, int b) { int res = a * b; return res; }
 }
