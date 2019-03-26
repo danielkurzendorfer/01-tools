38b0b22cbb2d9ac8e96966a5b727b379ec534860
Daniel Kurzendorfer <>
2019-03-26 08:25:09 +0100

    TODO entfernt

--- src/main/java/ohm/softa/a01/MyExampleClass.java
+++ src/main/java/ohm/softa/a01/MyExampleClass.java
@@ -1,12 +1,11 @@
 package ohm.softa.a01;
 
-public class MyExampleClass {
+class MyExampleClass {
 	/**
 	 * Basic integer multiplication
 	 * @return product of a and b
 	 */
-	public int multiply(int a, int b) {
-		// TODO return correct multiplication result
-		return a*b;
+    int multiply(int a, int b) {
+		return a * b;
 	}
 }
